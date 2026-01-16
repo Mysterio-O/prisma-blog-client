dc060ae8ec117f8dc837940cc4b07d7a25c73139
Mysterio-O <skrabbi.019@gmail.com>
2026-01-16 11:30:00 +0600

    added navbar from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Card className="bg-background">
+      <Card className="bg-background dark:bg-violet-400">
         <CardHeader>
           Card
         </CardHeader>
         <CardContent>
-          <Button
-          variant={"outline"}
-          >
-            Click
-          </Button>
+          <Link href={"/about"}>
+            <Button
+              variant={"outline"}
+            >
+              Click
+            </Button>
+          </Link>
         </CardContent>
       </Card>
     </div>
